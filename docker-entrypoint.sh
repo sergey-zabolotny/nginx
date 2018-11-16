40c777673aab60885b1d36aa2263032435fb0532
Sergey Zabolotny <sergey.zabolotny@ffwagency.com>
2018-11-16 10:39:02 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
 if [[ -n "${DEBUG}" ]]; then
@@ -12,31 +11,9 @@ _gotpl() {
     fi
 }
 
-# Backwards compatibility for old env vars names.
-_backwards_compatibility() {
-    declare -A vars
-    # vars[DEPRECATED]="ACTUAL"
-    vars[NGINX_ALLOW_XML_ENDPOINTS]="NGINX_DRUPAL_ALLOW_XML_ENDPOINTS"
-    vars[NGINX_STATIC_CONTENT_ACCESS_LOG]="NGINX_STATIC_ACCESS_LOG"
-    vars[NGINX_STATIC_CONTENT_EXPIRES]="NGINX_STATIC_EXPIRES"
-    vars[NGINX_STATIC_CONTENT_OPEN_FILE_CACHE]="NGINX_STATIC_OPEN_FILE_CACHE"
-    vars[NGINX_STATIC_CONTENT_OPEN_FILE_CACHE_MIN_USES]="NGINX_STATIC_OPEN_FILE_CACHE_MIN_USES"
-    vars[NGINX_STATIC_CONTENT_OPEN_FILE_CACHE_VALID]="NGINX_STATIC_OPEN_FILE_CACHE_VALID"
-    vars[NGINX_XMLRPC_SERVER_NAME]="NGINX_DRUPAL_XMLRPC_SERVER_NAME"
-    vars[NGINX_DRUPAL_TRACK_UPLOADS]="NGINX_TRACK_UPLOADS"
-
-    for i in "${!vars[@]}"; do
-        # Use value from old var if it's not empty and the new is.
-        if [[ -n "${!i}" && -z "${!vars[$i]}" ]]; then
-            export ${vars[$i]}="${!i}"
-        fi
-    done
-}
-
 process_templates() {
-    _backwards_compatibility
-
     _gotpl "nginx.conf.tmpl" "/etc/nginx/nginx.conf"
+    _gotpl "ssl.conf.tmpl" "/etc/nginx/ssl.conf"
     _gotpl "vhost.conf.tmpl" "/etc/nginx/conf.d/vhost.conf"
 
     _gotpl "includes/defaults.conf.tmpl" "/etc/nginx/defaults.conf"
@@ -55,13 +32,15 @@ process_templates() {
     _gotpl "50x.html.tmpl" "/usr/share/nginx/html/50x.html"
 }
 
-sudo init_volumes
-
 process_templates
-exec_init_scripts
 
-if [[ "${1}" == "make" ]]; then
-    exec "${@}" -f /usr/local/bin/actions.mk
-else
-    exec $@
+# Basic HTTP Authentication
+if [[ "$APACHE_BASIC_AUTH_USER" != "" ]] && [[ "$APACHE_BASIC_AUTH_PASS" != "" ]]; then
+	echo "Enabling Basic HTTP Authentication [$APACHE_BASIC_AUTH_USER:$APACHE_BASIC_AUTH_PASS]"
+	echo "$APACHE_BASIC_AUTH_USER:$(echo $APACHE_BASIC_AUTH_PASS | mkpasswd -m md5)" >/etc/nginx/htpasswd
+	echo 'auth_basic "Restricted area";' >/etc/nginx/conf.d/basic-auth.conf
+	echo 'auth_basic_user_file htpasswd;' >>/etc/nginx/conf.d/basic-auth.conf
 fi
+
+gen_ssl_certs /etc/nginx/ssl
+exec nginx
